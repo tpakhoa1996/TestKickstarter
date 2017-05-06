072269252718f2f7ee13feedcd9c70cbfbffd208
Tri Nguyen <mankinchi.1995@gmail.com>
2017-05-06 14:10:24 +1000

    Change style + Add todo list

--- controllers/test-api.controller.js
+++ controllers/test-api.controller.js
@@ -1,44 +1,43 @@
 let	kickstarter = require.main.require('./crawler/retrieveProject.js'),
-	url = require('url'),
+	url = require('url');
+module.exports = {
+	projectDisplay: (request, response) => {
+		console.log('Receive a GET request');
+		response.render('test-api', {
+			nav: 'api',
+			data: ''
+		});
+	},
 
-exports.projectDisplay = (request, response) => {
-	console.log('Receive a GET request');
-	response.render('test-api', {
-		nav: 'api',
-		data: ''
-	});
-};
-
-exports.projectSearch = (request, response) => {
-	console.log('Receive a POST request');
-	let urlOb = url.parse(request.body.link);
-	let projectLink = 'https://' + urlOb.host + urlOb.pathname;
-	console.log('Path: ' + projectLink);
-	kickstarter.crawler.queue([{
-		uri: projectLink, 
-		callback: function(err, res, done) {
-			if (err) {
-				console.log("Geting DOM Object error");
-				response.render('test-api', {
-					data: {error: true},
-					nav: 'api',
-					prev_search: projectLink,
-				});
+	projectSearch: (request, response) => {
+		console.log('Receive a POST request');
+		let urlOb = url.parse(request.body.link);
+		let projectLink = 'https://' + urlOb.host + urlOb.pathname;
+		console.log('Path: ' + projectLink);
+		kickstarter.crawler.queue([{
+			uri: projectLink,
+			callback: function(err, res, done) {
+				if (err) {
+					console.log("Geting DOM Object error");
+					response.render('test-api', {
+						data: {error: true},
+						nav: 'api',
+						prev_search: projectLink,
+					});
+				}
+				else {
+					console.log('Crawling ...');
+					let data = kickstarter.retrieveData(res);
+					data.projectLink = projectLink;
+					console.log(data.projectCompletionDate);
+					response.render('test-api', {
+						data: data,
+						nav : 'api',
+						prev_search: projectLink,
+					});
+				}
+				done();
 			}
-			else {
-				console.log('Crawling ...');
-				let data = kickstarter.retrieveData(res);
-				data.projectLink = projectLink;
-				console.log(data.projectCompletionDate);
-				response.render('test-api', {
-					data: data,
-					nav : 'api',
-					prev_search: projectLink,
-				});
-			}
-			done();
-		}
-	}]);
-};
-
-module.exports = exports;
+		}]);
+	}
+}
